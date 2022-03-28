387824e830d9456b8517bcdfa0a47411931b1b03
Alex Carmichael <carmichael.alexander@gmail.com>
2022-03-27 17:27:34 -0700

    adding mongodb functionality

--- 83596-CodeChallenge-CSHA/server.js
+++ 83596-CodeChallenge-CSHA/server.js
@@ -2,7 +2,8 @@
 'use strict';
 var path = require('path');
 var express = require('express');
-//const mysql = require("mysql");
+
+
 var app = express();
 
 var staticPath = path.join(__dirname, '/');
@@ -17,3 +18,32 @@ var server = app.listen(app.get('port'), function () {
 
 var frontEndAccessCount = 0;
 var lastFiveNames = [];
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+async function main() {
+    const uri = "mongodb+srv://REQ83596:iAWNYzSN4JKW8zqW@alexc-codechallenge.3o4eg.mongodb.net/sample_airbnb?retryWrites=true&w=majority";
+    const client = new MongoClient(uri);
+
+    try {
+        // Connect to the MongoDB cluster
+       await client.connect();
+
+        // Make the appropriate DB calls
+        await listDatabases(client);
+
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+async function listDatabases(client) {
+    databasesList = await client.db().admin().listDatabases();
+
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+main().catch(console.error);
