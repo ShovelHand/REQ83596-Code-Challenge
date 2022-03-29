d5689c3d136b8c6d4612c4aab1ebb8d1ef71dbc5
Alex Carmichael <carmichael.alexander@gmail.com>
2022-03-29 07:56:48 -0700

    Wrote installation README

--- 83596-CodeChallenge-CSHA/__tests__/front-end.test.ts
+++ 83596-CodeChallenge-CSHA/__tests__/front-end.test.ts
@@ -3,7 +3,13 @@ const app = require("../server"); // Link to your server file
 const supertest = require("supertest");
 const request = supertest(app);
 
-it("gets the logging endpoint", async () => {
+it("gets the logging endpoint. Make sure it returns an OK response", async (done) => {
     const response = await request.get("/logging");
     expect(response.status).toBe(200);
 });
+
+// it("sending a name to server updates tracking variables", async () => {
+//     const response = await request.post("/logging?name=Cpt.Janeway");
+//     expect(response.status).toBe(200);
+//  //   expect(request.lastFiveNames[0]).toBe("Cpt.Janeway");
+// });
